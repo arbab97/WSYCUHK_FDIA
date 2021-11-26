6714618f943341a66bd568daa06b5a7af9edc7c2
arbab97 <arbab.arshad97@gmail.com>
2021-11-27 02:45:19 +0400

    upd

--- Code/run_all_models.sh
+++ Code/run_all_models.sh
@@ -1,7 +1,7 @@
 
 declare epochs=50
 declare -a models=("MLP" "CNN" "LSTM" "Attention" "cnn-lstm-paper-experiments" )
-declare -a layers=(1 2 4)
+declare -a layers=(1 2)
 declare -a neurons=(64 128 256)
 
 ## now loop through the above array
@@ -14,11 +14,11 @@ do
       echo " Training ---------------------->  $model $layer $neuron   <----------------------------------------"
 
       export CUDA_VISIBLE_DEVICES=0 && source activate gpu && python /content/WSYCUHK_FDIA/Code/all_models.py  --model "$model" --n_epoch "$epochs" \
-      --data_dir "/content/drive/MyDrive/openUAE/locational detection/data_ieee14_118_locational_detection/data14_1.mat" \
-      --output_dir "/content/drive/MyDrive/openUAE/locational detection/tradeoff_analysis/IEEE14/" \
+      --data_dir "/content/drive/MyDrive/openUAE/locational detection/data_ieee14_118_locational_detection/data118_1.mat" \
+      --output_dir "/content/drive/MyDrive/openUAE/locational detection/tradeoff_analysis/IEEE118/" \
       --layers "$layer"\
       --neurons "$neuron"\
-      --shape 19
+      --shape 180
    # or do whatever with individual element of the array
     done
   done
